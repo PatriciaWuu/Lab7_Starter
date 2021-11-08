c89d13aecacb60a0252a4ab9f2afdbef29a1c78c
Patricia Wu <49573110+PatriciaWuu@users.noreply.github.com>
2021-11-07 22:27:08 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,14 +2,30 @@
 //         so do not move it next to the other scripts
 
 const CACHE_NAME = 'lab-7-starter';
-
+const recipesUrl = [
+  'https://introweb.tech/assets/json/ghostCookies.json',
+  'https://introweb.tech/assets/json/birthdayCake.json',
+  'https://introweb.tech/assets/json/chocolateChip.json',
+  'https://introweb.tech/assets/json/stuffing.json',
+  'https://introweb.tech/assets/json/turkey.json',
+  'https://introweb.tech/assets/json/pumpkinPie.json'
+];
 // Once the service worker has been installed, feed it some initial URLs to cache
 self.addEventListener('install', function (event) {
   /**
    * TODO - Part 2 Step 2
    * Create a function as outlined above
    */
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cacheInstall){
+        console.log('Cache opened!');
+        return cacheInstall.addAll(recipesUrl);
+    })
+
+  )
 });
+
 
 /**
  * Once the service worker 'activates', this makes it so clients loaded
@@ -21,6 +37,7 @@ self.addEventListener('activate', function (event) {
    * TODO - Part 2 Step 3
    * Create a function as outlined above, it should be one line
    */
+   event.waitUntil(clients.claim());
 });
 
 // Intercept fetch requests and store them in the cache
@@ -29,4 +46,14 @@ self.addEventListener('fetch', function (event) {
    * TODO - Part 2 Step 4
    * Create a function as outlined above
    */
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(respon){
+        if(respon){
+          return respon;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
 });
